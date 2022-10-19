c8d348d7a8a6d3ddcc8a67cf925bd21d35358329
Thuy PT <thuyptqnu@gmail.com>
2022-10-19 16:04:34 +0700

    up

---
+++ QLBAIGUIXE/QLBAIGUIXE/ViewModel/LoginViewModel.cs
@@ -0,0 +1,38 @@
+﻿using QLBAIGUIXE.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace QLBAIGUIXE.ViewModel
+{
+    class LoginViewModel : BaseViewModel
+    {
+        public bool IsLogin { get; set; }
+        private string _username;
+        private string _password;
+        public string Username { get => _username; set { _username = value; OnPropertyChanged(); } }
+        public string Password { get => _password; set { _password = value; OnPropertyChanged(); } }
+
+        public ICommand LoginCommand { get; set; }
+        //xu ly
+        public LoginViewModel()
+        {
+            IsLogin = false;
+            LoginCommand = new RelayCommand<Window>((p) => { return true; }, (p) => { IsLogin = true; });
+        }
+
+        void Login(Window p)
+        {
+            if (p == null)
+                return;
+
+            DataProvider.Ins.DB
+            IsLogin = true;
+            p.Close();
+        }
+    }
+}
